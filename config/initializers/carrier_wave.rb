9f806c98e5c5178d6304951d9abbcad442521d88
Joseph-Mwendwa <joseh.john@gmail.com>
2015-06-23 23:33:25 +0300

    correction

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAI75573NS5KWUSDUA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     : ENV['AKIAI75573NS5KWUSDUA'],
+      :aws_secret_access_key : ENV['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']
     }
     config.fog_directory     =  ENV['tik-tok']
   end
